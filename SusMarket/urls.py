52fc20018a848fad62627d934c6e231e2e75d029
RailTH <romandilio2008@gmail.com>
2024-02-01 19:46:44 +1100

    Added reset all browser styles & scss

--- SusMarket/urls.py
+++ SusMarket/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from SusMarketBackend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
+import './style.scss';
+import './reset.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
