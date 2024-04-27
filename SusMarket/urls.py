38b6991ca2b548de102e76af806066490b273869
KessPen <75772773+KessPenGames@users.noreply.github.com>
2024-04-27 15:00:49 +0700

    пофиксил ссылки

--- SusMarket/urls.py
+++ SusMarket/urls.py
@@ -20,6 +20,5 @@ from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('^.*$', TemplateView.as_view(template_name='index.html')),
-    path('api/', include("SusMarketBackend.urls"))
+    path('', include("SusMarketBackend.urls"))
 ]
--- SusMarketBackend/urls.py
+++ SusMarketBackend/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/category', views.category, name='category'),
-    path('get/products', views.products, name='products'),
-    path('get/reviews/<int:product>', views.reviews, name='reviews'),
-    path('post/user/<str:login>/<str:password>', views.register_user, name='register-user'),
-    path('post/check/user/<str:login>', views.check_user, name='check-user'),
-    path('get/user/<str:login>/<str:password>', views.user, name='get-user')
+    path('', views.index, name='index'),
+    path('info', views.index, name='info'),
+    path('profile', views.index, name='profile'),
+    path('product', views.index, name='product'),
+    path('payment', views.index, name='payment'),
+    path('scam', views.index, name='scam'),
+    path('api/get/category', views.category, name='category'),
+    path('api/get/products', views.products, name='products'),
+    path('api/get/reviews/<int:product>', views.reviews, name='reviews'),
+    path('api/post/user/<str:login>/<str:password>', views.register_user, name='register-user'),
+    path('api/post/check/user/<str:login>', views.check_user, name='check-user'),
+    path('api/get/user/<str:login>/<str:password>', views.user, name='get-user')
 ]
