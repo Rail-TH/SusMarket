b0d6dd16bbdeb04e6660f181a6f5fd954cdc44d1
RailTH <romandilio2008@gmail.com>
2024-04-27 19:43:55 +1100

    Fixing product page
    Recoding text shorting from css to script code & other fixes of styles and html code

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -7,7 +7,7 @@ import InfoPage from "./pages/InfoPage";
 import Header from "./components/Header";
 import PopupMap from "./components/PopupMap";
 import React, { useState, useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Product, Category } from "./utils/types";
 import axios from 'axios';
 
