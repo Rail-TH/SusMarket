e9849a12323d456ea331aa7e6692d3d956848302
KessPen <lmityt47@gmail.com>
2024-04-30 12:24:56 +0700

    Добавил модели в админку

--- SusMarketBackend/admin.py
+++ SusMarketBackend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Product, User, Review
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(User)
+admin.site.register(Review)
