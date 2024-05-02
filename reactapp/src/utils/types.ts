afeaa330b726d5d280350532428b08d2b6531c85
RailTH <romandilio2008@gmail.com>
2024-05-02 23:52:15 +1100

    Api request for categories

--- reactapp/src/utils/types.ts
+++ reactapp/src/utils/types.ts
@@ -1,9 +1,13 @@
-export type Category = 'toys' | 'clothes' | 'figures' | 'foods' | 'books';
-
 export interface Product {
   id: number;
   title: string;
-  category: Category;
+  category: string;
   price: number;
   image: any;
+}
+
+export interface Category {
+  id: string;
+  title: string;
+  image: string;
 }
