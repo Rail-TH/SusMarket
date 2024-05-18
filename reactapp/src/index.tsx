9519f45c5fb04c8c80cee5e0f9da20fc21584193
RailTH <romandilio2008@gmail.com>
2024-05-18 23:06:33 +1100

    Added StrictMode

--- reactapp/src/index.tsx
+++ reactapp/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
@@ -7,9 +8,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
