77a2364eceece0998332cd9f02ed7e3f4614c6a3
RailTH <romandilio2008@gmail.com>
2024-05-20 20:16:09 +1100

    Added button for logout account in profile page

--- reactapp/src/pages/ProfilePage.tsx
+++ reactapp/src/pages/ProfilePage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Banner from "../components/AdBanner";
+import { motion } from "framer-motion";
 import '../ProfileStyle.scss';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import ProfileOrders from "../components/ProfileOrders";
 import ProfilePurchases from "../components/ProfilePurchases";
+import LogoutIcon from "../assets/icons/logout-icon.svg";
+import Cookies from "js-cookie";
 
 export default function ProfilePage() {
+    const navigate = useNavigate();
+    
+    const handleLogout = () => {
+        Cookies.remove('user');
+        Cookies.remove('user_id');
+        navigate('/');
+    }
+    
     return(
         <section className="profile-page">
             <Banner />
@@ -13,6 +24,14 @@ export default function ProfilePage() {
                 <Route path="/" element={<ProfileOrders/>}/>
                 <Route path="purchases" element={<ProfilePurchases/>}/>
             </Routes>
+            <motion.button 
+                className="profile-page__logout-button"
+                whileTap={{scale: 0.9}}
+                transition={{duration: 0.2, type: "spring"}}
+                onClick={handleLogout}
+            >
+                <img src={LogoutIcon} alt="Logout icon" />
+            </motion.button>
         </section>
     )
 }
